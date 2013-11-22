fc73a96da452840afe0b0692ba1671c3ab8ec41d
jielu <jielu@gatech.edu>
2013-11-21 22:14:53 -0500

    TestRunnerMapper ready

--- Tarantula_MR/src/edu/gatech/cs6422/TestRunnerMapper.java
+++ Tarantula_MR/src/edu/gatech/cs6422/TestRunnerMapper.java
@@ -1,50 +1,127 @@
 package edu.gatech.cs6422;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
 
 public class TestRunnerMapper<K> extends MapReduceBase implements
 		Mapper<K, Text, IntWritable, Text> {
 	// Input: Value (Text): fully qualified test class name
 	// Output: Key (IntWritable): basic block id
-	// Output: Value (Text): covered test case(method) name and test result, e.g. test1.test1:P
-	
+	// Output: Value (Text): covered test case(method) name and test result,
+	// e.g. test1.test1:P, test1.test1:F
+
 	private String junit3Lib;
 	private String junit4Lib;
 	private String classPath;
-	
-	public void configure(JobConf job){
+	private URLClassLoader loader;
+
+	private JUnitCore junitCore;
+
+	public void configure(JobConf job) {
 		junit3Lib = job.get("testRunnerMapper.junit3.lib");
 		junit4Lib = job.get("testRunnerMapper.junit4.lib");
 		classPath = job.get("testRunnerMapper.classpath");
+
+		try {
+			loader = new URLClassLoader(new URL[] { new URL(classPath),
+					new URL(junit3Lib), new URL(junit4Lib) });
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void map(K key, Text value, OutputCollector<IntWritable, Text> output,
-			Reporter reporter) throws IOException {
-		// Add tested program, test cases, junit library to classpath
-		URLClassLoader loader = new URLClassLoader(new URL[]
-				{new URL(classPath), new URL(junit3Lib), new URL(junit4Lib)});
+	public void map(K key, Text value,
+			OutputCollector<IntWritable, Text> output, Reporter reporter)
+			throws IOException {
+		junitCore = new JUnitCore();
+
 		try {
 			// Load test case class
 			Class testClass = Class.forName(value.toString(), true, loader);
-			
-			// Figure out junit3 or junit4
-			
-			
+			boolean isJunit3 = testClass.getSuperclass().getName().equals("junit.framework.TestCase");
+
+			Method[] methods = testClass.getMethods();
+			for(int i=0; i<methods.length; i++){
+				Method method = methods[i];
+				String methodName = method.getName();
+				if(isJunit3){
+					if(method.getModifiers() == Modifier.PUBLIC && methodName.startsWith("test")){
+						runTestMethod(testClass, methodName, output);
+					}
+				}else{
+					org.junit.Test annotation = method.getAnnotation(org.junit.Test.class);
+					if (annotation != null) {
+						runTestMethod(testClass, methodName, output);
+					}
+				}
+			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
 		}
-		
+
+	}
+
+	// Run test method and get coverage data
+	private void runTestMethod(Class testClass, String methodName,
+			OutputCollector<IntWritable, Text> output) {
+		Request request = Request.method(testClass, methodName);
+		Result testResult = junitCore.run(request);
+
+		try {
+			Class bm = Class.forName("insect.coverage.execution.BlockMonitor",
+					true, loader);
+
+			Field f = (Field) bm.getDeclaredField("blocks");
+			f.setAccessible(true);
+			long[] coverage = (long[]) f.get(null);
+
+			for (int j = 0; j < coverage.length; j++) {
+				if (coverage[j] > 0) {
+					StringBuilder test = new StringBuilder();
+					test.append(testClass.getName());
+					test.append(".");
+					test.append(methodName);
+					test.append(":");
+					test.append(testResult.wasSuccessful() == true ? "P" : "F");
+					output.collect(new IntWritable(j),
+							new Text(test.toString()));
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
